ac8cf76eed247b17644c31c69f5be05f2f55e82d
Sunbridger <739272884@qq.com>
2019-07-07 22:52:20 +0800

    使用classProperties简化代码

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,13 @@ import Context from './components/context/context.jsx';
 import ContextSumer from './components/context/context-consumer.jsx';
 import {ThemeContext, themes} from './components/context/theme-context.js';
 export default class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            interf:{
-                theme:themes.light,
-                changeStyle:()=>{
-                    alert('可以改变颜色逻辑写着')
-                }
+    state = {
+        interf:{
+            theme:themes.light,
+            changeStyle:()=>{
+                alert('可以改变颜色逻辑写着')
             }
-        };
+        }  
     }
     render() {
         // 这样写容易造成每一次 Provider 重渲染时，以下的代码会重渲染所有下面的 consumers 组件
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                     loader:'babel-loader',
                     options:{
                         "presets": [["@babel/preset-env",{"useBuiltIns":"usage","corejs":2}],"@babel/preset-react"],
-                        "plugins": ["@babel/plugin-transform-runtime"]
+                        "plugins": ["@babel/plugin-transform-runtime","@babel/plugin-proposal-class-properties"]
                     }
                 }
             }
